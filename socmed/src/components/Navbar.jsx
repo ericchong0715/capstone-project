e9e4122d80cc05af92848c06bd00aea175a08078
ericchong0715 <ericchong0715@gmail.com>
2025-08-30 01:24:03 +0800

    fix: Ensure user dropdown is always visible on small screens and hide toggler on large screens

--- socmed/src/components/Navbar.jsx
+++ socmed/src/components/Navbar.jsx
@@ -19,8 +19,22 @@ export default function Navbar() {
             SocMed
           </Link>
         </div>
+        {user && (
+              <li className="nav-item dropdown d-flex d-lg-none ">
+                <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown">
+                  {user.name}
+                </a>
+                <ul className="dropdown-menu dropdown-menu-end">
+                  <li>
+                    <button className="dropdown-item" onClick={handleLogout}>
+                      Logout
+                    </button>
+                  </li>
+                </ul>
+              </li>
+            )}
         {/* Right side: Collapsible menu with user/login/register */}
-        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#nav" aria-controls="nav" aria-expanded="false" aria-label="Toggle navigation">
+        <button className="navbar-toggler d-lg-none" type="button" data-bs-toggle="collapse" data-bs-target="#nav" aria-controls="nav" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
         </button>
         <div id="nav" className="collapse navbar-collapse">
@@ -37,7 +51,7 @@ export default function Navbar() {
             )}
 
             {user && (
-              <li className="nav-item dropdown">
+              <li className="nav-item dropdown d-none d-lg-flex">
                 <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown">
                   {user.name}
                 </a>
